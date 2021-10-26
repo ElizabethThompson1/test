b6d8d9e37e660c96b6934acd53e85317b9e20a6e
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-10-26 15:32:23 -0500

    added a loop that goes through both function until you are happy with the trip.

--- Daytrip.js
+++ Daytrip.js
@@ -1,50 +1,91 @@
 "use strick"
-let destination = ["Utah", "California","Las Vegas", "Colorado", "Arizona"]
-let restaurants = ["Heart Attack Grill","Picnic","Sushi","Nacho Daddy","Bar and Grill"]
-let transportation = ["walk","car","Uber","bike","bus"]
-let activities = ["Watch a Movie", "Go on a Hike", "Sports event", "Dancing", "Paintballing"]
-
-function randomDestination(location){
-   let randomNumber = Math.floor(Math.random() *5)
-   let destinationSelection = location[randomNumber];
-   return destinationSelection 
-}
-let selectedDestination = randomDestination(destination)
+let area= ["Utah", "California","Las Vegas", "Colorado", "Arizona"]
+let food = ["Heart Attack Grill","Picnic","Sushi","Nacho Daddy","Bar and Grill"]
+let trans = ["walk","car","Uber","bike","bus"]
+let thingsToDo = ["Watch a Movie", "Go on a Hike", "Sports event", "Dancing", "Paintballing"]
 
 
-// console.log(Math.floor(Math.random() *5 + 1)) gives me a random numner
-// console.log(destination[Math.floor(Math.random() *5)]) 
-
-function randomRestaurant (place){
-    let randomNumberFood = Math.floor(Math.random() *5)
-    let restaurantOption = place[randomNumberFood];
-    return restaurantOption
-}
-let selectedRestaurant = randomRestaurant(restaurants)
-
-
-function randomtransportation(howToGetAround){
-    let numberTransportation = Math.floor(Math.random() *5)
-    let transportationOptions= howToGetAround[numberTransportation];
-    return transportationOptions
-}
-let selectedTransportation = randomtransportation(transportation)
-n)
-
-function randomactivities (funThingsToDo){
-    let randomNumberActivities = Math.floor(Math.random() *5)
-    let activitiesOptions = funThingsToDo[randomNumberActivities]
-    return activitiesOptions
-}
-let selectedactivities = randomactivities(activities)
-
-
-function dayTripWasSelectedOrNot (answer){
-    if(answer === "yes"){
-        console.log("The Perfect Daytrip!")}
-     else{
-       console.log ("Let's try again!")
-        }
+ function randomSelection(array){
+    return array [Math.floor(Math.random() *array.length)]
+    }
+    let selectedDestination="1.Destination"+" "+ randomSelection(area);
     
+    let selectedRestaurant="2.Restaurant"+" "+ randomSelection(food);
+    
+    let selectedTransportation="3.Transportation"+" "+ randomSelection(trans);
+    
+    let selectedactivities="4.Activity"+" "+ randomSelection(thingsToDo);
+    
+
+
+    let arr = [selectedDestination, selectedRestaurant, selectedTransportation,selectedactivities]
+        let finalOption=""
+        for (let i = 0; i< arr.length; i++){
+        finalOption += arr[i] + "\n";
+        }
+
+
+    function pickFeatureToChange () {
+    let Answer = prompt("what would you like to change?");
+    if(Answer === "Destination" ||Answer ===  "1" ||Answer ===  "1.Destination" ){
+        selectedDestination = ("1.Destination" +" " +randomSelection(area))
+            let arr = [selectedDestination, selectedRestaurant, selectedTransportation,selectedactivities]
+            let finalOption=""
+            for (let i = 0; i< arr.length; i++)
+            finalOption += arr[i] + "\n";
+
+            alert(finalOption);
+        }
+    else if  (Answer === "Restaurant" ||Answer ===  "2" ||Answer ===  "2.Resturant"){
+            selectedRestaurant = ("2. Resturant" +" " +randomSelection(food))
+            let arr = [selectedDestination, selectedRestaurant, selectedTransportation,selectedactivities]
+            let finalOption=""
+            for (let i = 0; i< arr.length; i++)
+            finalOption += arr[i] + "\n";
+
+            alert(finalOption)
+        }
+     else if (Answer === "Transportation" ||Answer ===  "3" ||Answer ===  "3.Transportation"){
+            selectedTransportation = ("3.Transportation" +" " +randomSelection(trans))
+            let arr = [selectedDestination, selectedRestaurant, selectedTransportation,selectedactivities]
+            let finalOption=""
+            for (let i = 0; i< arr.length; i++)
+            finalOption += arr[i] + "\n";
+
+            alert(finalOption) 
+        }  
+    else if  (Answer === "Activity" ||Answer ===  "4" ||Answer ===  "4.Activity"){
+            selectedactivities =("4.Activity" + " "+ randomSelection(thingsToDo))
+            let arr = [selectedDestination, selectedRestaurant, selectedTransportation,selectedactivities]
+            let finalOption=""
+            for (let i = 0; i< arr.length; i++)
+            finalOption += arr[i] + "\n";
+
+            alert(finalOption)
+
+        }
+    else {
+            pickFeatureToChange
+        }
+    }
+    alert(finalOption);
+ function checkForTripSatisfaction (){
+     let whenComplete = false;
+     while(whenComplete === false){
+
+        
+
+        let question = prompt("Would you like to change anything? yes or no?");
+
+        if (question === "no"){ 
+            whenComplete = true;
+
+            alert(finalOption);
+        }
+            
+        else {(question === "yes");
+            pickFeatureToChange();
+        }
+    }    
 }
-dayTripWasSelectedOrNot(prompt("Do you like this day trip? Yes or No?"))
+checkForTripSatisfaction();
